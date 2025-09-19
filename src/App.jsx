4c21d52c14751813117e3e317c9c16861b5fe44f
Anand-1812 <anand.work2005@gmail.com>
2025-09-19 16:40:13 +0530

    add: home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
 import Navbar from "./Navbar/Navbar"
+import Home from "./Home/Home"
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="bg-gray-50 min-h-screen">
+    <div className="bg-gray-50 min-h-screen flex flex-col">
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="flex-1">
+        <Home />
+      </div>
     </div>
   )
 }

3cc9779c5d51f10d39f11c1ab76a96e6608d2832
Anand-1812 <anand.work2005@gmail.com>
2025-10-15 12:17:12 +0530

    connection with mongo and express done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,21 @@
 import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
+import express from "express";
 
 // connection with the databse
-dotenv.config()
+dotenv.config();
+
+// express
+const PORT = process.env.APP_PORT || 7123
+const app = express();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDb connected."))
   .catch(err => console.log(`Error: ${err}`))
 
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+})
 
 

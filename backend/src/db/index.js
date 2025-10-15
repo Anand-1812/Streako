8ebfa803fd12bebc3caae478e36b3b9380cc7433
Anand-1812 <anand.work2005@gmail.com>
2025-10-15 12:42:22 +0530

    add user models

---
+++ backend/src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("mongoDB connection successful");
+
+  } catch (error) {
+    console.log(`MongoDB connection failed: ${err}`);
+  }
+}
+
+export default connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,27 @@
 import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./db/index.js";
 
 // connection with the databse
-dotenv.config();
+dotenv.config({
+  path: "./.env",
+});
 
 // express
 const PORT = process.env.APP_PORT || 7123
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDb connected."))
-  .catch(err => console.log(`Error: ${err}`))
-
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-})
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`mongoDB connection error: ${err}`);
+    process.exit(1);
+  })
 
 

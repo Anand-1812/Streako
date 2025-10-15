f23c990d84b0f02b1a518c777c12e3382efe8fe2
Anand-1812 <anand.work2005@gmail.com>
2025-10-15 11:23:19 +0530

    managing folder

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router'
 import Home from './Home/Home.jsx'
 import About from './About/About.jsx'
-import Dashboard from './Dashboard/Dashboard.jsx'
-import Signup from './components/Auth/Signup.jsx'
-import Login from './components/Auth/Login.jsx'
+import Dashboard from "./components/Dashboard/Dashboard.jsx"
 
 const router = createBrowserRouter(
   [
@@ -19,8 +17,6 @@ const router = createBrowserRouter(
         { path: 'home', element: <Home /> },
         { path: 'about', element: <About /> },
         { path: 'dashboard', element: <Dashboard /> },
-        { path: 'signup', element: <Signup /> },
-        { path: 'login', element: <Login /> }
       ]
     }
   ]
